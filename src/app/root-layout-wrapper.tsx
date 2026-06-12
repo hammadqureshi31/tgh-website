2f5469a21b19b14650d8e11ec5301d2297a60dd9
hammadqureshi31 <hammad2004qureshi@gmail.com>
2026-06-12 10:14:31 +0500

    add GA4 tag

--- src/app/root-layout-wrapper.tsx
+++ src/app/root-layout-wrapper.tsx
@@ -10,6 +10,21 @@ interface RootLayoutWrapperProps {
 export default function RootLayoutWrapper({ children }: RootLayoutWrapperProps) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-71PJRNLKZY"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-71PJRNLKZY');
+            `,
+          }}
+        />
+      </head>
       <GoogleTagManager gtmId="GTM-T76SSRM7" />
       <body className="antialiased" suppressHydrationWarning>
         {children}
